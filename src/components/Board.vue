aba99d6f1cf5cea4382791877fa2c7af398b2099
Spencer Fitzgerald <spencerfitz0196@gmail.com>
2020-03-10 18:27:27 -0600

    finished 'time travel'

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,64 +1,32 @@
 <template>
   <div>
-    <div class="status">{{ status }}</div>
     <div class="board-row">
-      <Square :value="this.squares[0]" @updateSquare="handleClick(0)" />
-      <Square :value="this.squares[1]" @updateSquare="handleClick(1)" />
-      <Square :value="this.squares[2]" @updateSquare="handleClick(2)" />
+      <Square :value="this.squares[0]" @updateSquare="$emit('updateSquare', 0)" />
+      <Square :value="this.squares[1]" @updateSquare="$emit('updateSquare', 1)" />
+      <Square :value="this.squares[2]" @updateSquare="$emit('updateSquare', 2)" />
     </div>
     <div class="board-row">
-      <Square :value="this.squares[3]"  @updateSquare="handleClick(3)" />
-      <Square :value="this.squares[4]"  @updateSquare="handleClick(4)" />
-      <Square :value="this.squares[5]"  @updateSquare="handleClick(5)" />
+      <Square :value="this.squares[3]" @updateSquare="$emit('updateSquare', 3)" />
+      <Square :value="this.squares[4]" @updateSquare="$emit('updateSquare', 4)" />
+      <Square :value="this.squares[5]" @updateSquare="$emit('updateSquare', 5)" />
     </div>
     <div class="board-row">
-      <Square :value="this.squares[6]"  @updateSquare="handleClick(6)" />
-      <Square :value="this.squares[7]"  @updateSquare="handleClick(7)" />
-      <Square :value="this.squares[8]"  @updateSquare="handleClick(8)" />
+      <Square :value="this.squares[6]" @updateSquare="$emit('updateSquare', 6)" />
+      <Square :value="this.squares[7]" @updateSquare="$emit('updateSquare', 7)" />
+      <Square :value="this.squares[8]" @updateSquare="$emit('updateSquare', 8)" />
     </div>
   </div>
 </template>
 
 <script>
 import Square from './Square';
-import {calculateWinner} from '../utilities/helperFunctions';
 
 export default {
   name: "Board",
   components: {
     Square
   },
-  data: function() {
-    return {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  },
-  computed: {
-    status: function() {
-      const winner = calculateWinner(this.squares);
-      let status;
-      if (winner) {
-        status = 'Winner: ' + winner;
-      } else {
-        status = 'Next player: ' + (this.xIsNext ? 'X' : 'O');
-      }
-      return status;
-    }
-  },
-  methods: {
-    handleClick(i) {
-      const newSquares = this.squares.slice(); // Need to slice data here instead of modifying index of array directly, otherwise reactivity system can't update
-      if (calculateWinner(newSquares) || newSquares[i]) {
-        return;
-      }
-      newSquares[i] = this.xIsNext ? 'X' : 'O';
-
-      // Reset state
-      this.squares = newSquares;
-      this.xIsNext = !this.xIsNext;
-    }
-  }
+  props: ['squares'],
 }
 </script>
 
--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,23 +1,84 @@
 <template>
     <div class="game">
     <div class="game-board">
-        <Board />
+        <Board
+            :squares="current.squares"
+            @updateSquare="handleClick"
+          />
     </div>
     <div class="game-info">
-        <div></div>
-        <ol></ol>
+        <div>{{ status }}</div>
+        <ol>
+          <li v-for="(step, move) of history" :key="move">
+            <button 
+              @click="jumpTo(move)"
+            >
+              {{ move ? 'Go to move #' + move : 'Go to game start'}}
+            </button>
+          </li>
+        </ol>
     </div>
     </div>
 </template>
 
 <script>
-import Board from './Board.vue'
+import Board from './Board.vue';
+import {calculateWinner} from '../utilities/helperFunctions';
 
 export default {
-    name: "Game",
-    components: {
-      Board
+  name: "Game",
+  components: {
+    Board
+  },
+  data: function() {
+    return {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      stepNumber: 0,
+      xIsNext: true,
     }
+  },
+  computed: {
+    current: function() {
+      return this.history[this.stepNumber];
+    },
+    status: function() {
+      const winner = calculateWinner(this.current.squares);
+
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.xIsNext ? 'X' : 'O');
+      }
+
+      return status;
+    }
+  },
+  methods: {
+    handleClick(i) {
+      const history = this.history.slice(0, this.stepNumber + 1);
+      const current = this.current;
+      const squares = current.squares.slice();
+      
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      
+      squares[i] = this.xIsNext ? 'X' : 'O';
+
+      this.history = history.concat([{
+        squares: squares,
+      }]),
+      this.stepNumber = history.length
+      this.xIsNext = !this.xIsNext
+    },
+    jumpTo(step) {
+      this.stepNumber = step;
+      this.xIsNext = (step % 2) === 0;
+    }
+  }
 }
 </script>
 
